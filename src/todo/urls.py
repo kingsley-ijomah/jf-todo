e64759503d69b30ad2ecd9e2db4d50a56403d50a
kingsley ijomah <kingsley.ijomah@gmail.com>
2020-07-24 19:31:03 +0100

    adding views templates url

---
+++ src/todo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import delete_todo, list_todo, new_todo, update_todo
+
+urlpatterns = [
+    path("", list_todo, name="list-todo"),
+    path("new-todo", new_todo, name="new-todo"),
+    path("update-todo/<int:pk>", update_todo, name="update-todo"),
+    path("delete-todo/<int:pk>", delete_todo, name="delete-todo"),
+]
--- src/todo/views.py
+++ src/todo/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def list_todo(request):
+    return render(request, "list_todo.html", context={})
+
+
+def new_todo(request):
+    return render(request, "new_todo.html", context={})
+
+
+def update_todo(request, pk):
+    return render(request, "update_todo.html", context={})
+
+
+def delete_todo(request, pk):
+    return render(request, "delete_todo.html", context={})
